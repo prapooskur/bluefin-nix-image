afa604a87951ee7530bdb7b9e521ce01db3fcddc
Prasiddh <61389919+prapooskur@users.noreply.github.com>
2025-10-12 15:28:57 -0700

    btop

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # add preferred packages to image
-dnf5 install -y nmtui
+dnf5 install -y nmtui btop
 
 # Use a COPR Example:
 #
