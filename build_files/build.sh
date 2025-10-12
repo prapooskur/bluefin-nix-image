34f51336fd46411c77d8b7f0a00df1f61ae39c86
Prasiddh Pooskur <61389919+prapooskur@users.noreply.github.com>
2025-10-11 21:34:12 -0700

    add cosmic (clueless)

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# add nmtui to image
+# add preferred packages to image
 dnf5 install -y nmtui
 
 # Use a COPR Example:
@@ -23,10 +23,12 @@ dnf5 install -y nmtui
 dnf5 -y copr enable lukenukem/asus-linux
 dnf5 -y install asusctl asusctl-rog-gui
 
-#### Example for enabling a System Unit File
+# add cosmic (this may be a horrible idea)
+dnf5 -y copr enable ryanabx/cosmic-epoch
+dnf5 -y install cosmic-desktop
 
-systemctl enable podman.socket
 
+# nix setup (may  not actually do anything on silverblue 41 base)
 mkdir -p /nix && \
 	curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix -o /nix/determinate-nix-installer.sh && \
 	chmod a+rx /nix/determinate-nix-installer.sh
